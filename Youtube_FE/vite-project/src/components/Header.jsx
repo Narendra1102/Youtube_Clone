8a5c91e4d9c4f8b5f335848684430de7685f5b0f
Narendra1102 <narendra.v02111@gmail.com>
2026-04-12 22:14:31 +0530

    Added Header

---
+++ Youtube_FE/vite-project/src/components/Header.jsx
@@ -0,0 +1,59 @@
+import { FaBars, FaSearch, FaUserCircle } from "react-icons/fa";
+import { useAuth } from "../context/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import ProfileDropdown from "./ProfileDropdown";
+import SignInModal from "./SignInModal";
+import youtubeLogo from "../assets/youtube logo.png";
+
+const Header = ({ toggleSidebar }) => {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+  const [showSignIn, setShowSignIn] = useState(false);
+
+  const handleSearch = () => {
+    navigate(`/?search=${query}`);
+  };
+
+  return (
+    <div className="header">
+      <FaBars className="icon" onClick={toggleSidebar} />
+
+      <Link to="/" className="logo">
+        <img src={youtubeLogo} alt="YouTube Logo" className="logoImage" />
+      </Link>
+
+      <div className="searchBox">
+        <input
+          placeholder="Search videos..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <FaSearch onClick={handleSearch} style={{ cursor: "pointer" }} />
+      </div>
+
+      {user ? (
+        <div className="headerRight">
+          <Link to="/upload">
+            <button className="uploadBtn">Upload</button>
+          </Link>
+          <ProfileDropdown />
+        </div>
+      ) : (
+        <>
+          <button className="loginBtn" onClick={() => setShowSignIn(true)}>
+            <FaUserCircle /> Sign In
+          </button>
+          <Link to="/upload">
+            <button className="uploadBtn">Upload</button>
+          </Link>
+        </>
+      )}
+
+      <SignInModal isOpen={showSignIn} onClose={() => setShowSignIn(false)} />
+    </div>
+  );
+};
+
+export default Header;
