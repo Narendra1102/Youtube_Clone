57c8b7c8b441439538e17fc503e819feef66cf75
Narendra1102 <narendra.v02111@gmail.com>
2026-04-12 22:36:35 +0530

    Added register page

---
+++ Youtube_FE/vite-project/src/pages/Register.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { registerUser } from "../api/authApi";
+import { useNavigate, Link } from "react-router-dom";
+
+const Register = () => {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) =>
+    setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await registerUser(form);
+      alert("Registered! Please login.");
+      navigate("/login");
+    } catch (err) {
+      alert("Registration failed");
+    }
+  };
+
+  return (
+    <div className="authPage">
+      <h2>Register</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input name="name" placeholder="Name" onChange={handleChange} required />
+        <input name="email" placeholder="Email" onChange={handleChange} required />
+        <input name="password" type="password" placeholder="Password" onChange={handleChange} required />
+        <button>Register</button>
+      </form>
+
+      <p>Already have account? <Link to="/login">Login</Link></p>
+    </div>
+  );
+};
+
+export default Register;
